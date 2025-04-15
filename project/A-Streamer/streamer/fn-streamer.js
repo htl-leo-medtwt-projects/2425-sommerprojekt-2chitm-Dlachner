73ab7e6dbb75324ae5ed726cb754dbe573877d1d
Dlachner <d.lachner@students.htl-leonding.ac.at>
2025-04-15 09:45:03 +0200

    Kleines Update:   -Streamer haben nun die Kategorie "Stream" wo deren Streams später gespeichert werden
      -Erster Stream von Jynxzi eingefügt

--- project/A-Streamer/streamer/fn-streamer.js
+++ project/A-Streamer/streamer/fn-streamer.js
@@ -4,83 +4,95 @@ const fn_streamer = [
         "Follower": 16700000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/kaicenat.jpeg"
+        "Pf": "../A-Streamer/pf/kaicenat.jpeg",
+        "Stream": ""
     },
     {
         "Name": "Tfue",
         "Follower": 11300000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/tfue.jng"
+        "Pf": "../A-Streamer/pf/tfue.jng",
+        "Stream": ""
     },
     {
         "Name": "Mongraal",
         "Follower": 7100000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/mongraal.jpeg"
+        "Pf": "../A-Streamer/pf/mongraal.jpeg",
+        "Stream": ""
     },
     {
         "Name": "Ninja",
         "Follower": 19200000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/ninja.jpg"
+        "Pf": "../A-Streamer/pf/ninja.jpg",
+        "Stream": ""
     },
     {
         "Name": "Bugha",
         "Follower": 5500000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/bugha.jpeg"
+        "Pf": "../A-Streamer/pf/bugha.jpeg",
+        "Stream": ""
     },
     {
         "Name": "MrSavage",
         "Follower": 4500000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/mrsavage.png"
+        "Pf": "../A-Streamer/pf/mrsavage.png",
+        "Stream": ""
     },
     {
         "Name": "Trymacs",
         "Follower": 3700000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/trymacs.png"
+        "Pf": "../A-Streamer/pf/trymacs.png",
+        "Stream": ""
     },
     {
         "Name": "StandartSkill",
         "Follower": 3500000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/standartskill.png"
+        "Pf": "../A-Streamer/pf/standartskill.png",
+        "Stream": ""
     },
     {
         "Name": "Oatley",
         "Follower": 645000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/oatley.png"
+        "Pf": "../A-Streamer/pf/oatley.png",
+        "Stream": ""
     },
     {
         "Name": "NickEh30",
         "Follower": 5700000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/nickeh30.png"
+        "Pf": "../A-Streamer/pf/nickeh30.png",
+        "Stream": ""
     },
     {
         "Name": "Clix",
         "Follower": 8100000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/clix.png"
+        "Pf": "../A-Streamer/pf/clix.png",
+        "Stream": ""
     },
     {
         "Name": "ohnePixel",
         "Follower": 1700000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/ohnepixel.webp"
+        "Pf": "../A-Streamer/pf/ohnepixel.webp",
+        "Stream": ""
     }
-]
+];
--- project/A-Streamer/streamer/r6-streamer.js
+++ project/A-Streamer/streamer/r6-streamer.js
@@ -4,48 +4,55 @@ const r6_streamer = [
         "Follower": 7100000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/jynxzi.jpg"
+        "Pf": "../A-Streamer/pf/jynxzi.jpg",
+        "Stream": "../streams/jynxzi1.mp4"
     },
     {
         "Name": "CaseOh",
         "Follower": 6800000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/caseoh.webp"
+        "Pf": "../A-Streamer/pf/caseoh.webp",
+        "Stream": ""
     },
     {
         "Name": "itsSpoit",
         "Follower": 821000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/itsSpoit.png"
+        "Pf": "../A-Streamer/pf/itsSpoit.png",
+        "Stream": ""
     },
     {
         "Name": "shroud",
         "Follower": 11100000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/shroud.jpg"
+        "Pf": "../A-Streamer/pf/shroud.jpg",
+        "Stream": ""
     },
     {
         "Name": "thesketchreal",
         "Follower": 1500000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/thesketchreal.png"
+        "Pf": "../A-Streamer/pf/thesketchreal.png",
+        "Stream": ""
     },
     {
         "Name": "Pengu",
         "Follower": 992000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/pengu.png"
+        "Pf": "../A-Streamer/pf/pengu.png",
+        "Stream": ""
     },
     {
         "Name": "KingGeorge",
         "Follower": 1100000,
         "Status": false,
         "Liveviewer": 0,
-        "Pf": "../A-Streamer/pf/kinggeorge.jpg"
+        "Pf": "../A-Streamer/pf/kinggeorge.jpg",
+        "Stream": ""
     }
-]
+];
